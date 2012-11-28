e01075aa0f8f79b7678d62afe059f681fe1b8388
unknown <hrd@.Nesstr.com>
2012-11-28 20:50:22 +0200

    20/12/2012

---
+++ php/pages/first.php
@@ -0,0 +1,56 @@
+<?php
+
+	
+
+require 'html/header.php';
+
+
+	
+
+?>
+
+<div id="main">
+	<header>
+		<h1>Title</h1>
+	</header>
+	<div id="content">
+		<aside>
+			<nav>
+				<ul>
+					<li class="btn">
+						<a href="">menu link</a>
+					</li>
+					<li class="btn">
+						<a href="">menu link</a>
+					</li>
+					<li class="btn">
+						<a href="">menu link</a>
+					</li>
+					<li class="btn">
+						<a href="">menu link</a>
+					</li>
+					<li class="btn">
+						<a href="">menu link</a>
+					</li>
+				</ul>
+			</nav>
+		</aside>
+		<article>
+			<h1>title</h1>
+			<p>
+			drgrdsdsg s dg  ssegdsgtgfjhtfyhdf
+			xdrhgtftft
+			sfdthdfttfttff
+			sdrhgdf
+			</p>
+		</article>
+		
+		
+		
+	</div>
+</div>
+<?php
+
+require 'html/footer.php';
+?>
+
---
+++ php/pages/html/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		
+		<link rel="stylesheet" type="text/css" href="style/css/bootstrap.css">
+		
+		<link rel="stylesheet" type="text/css" href="style/css/style.css">
+		
+		<title>Progect </title>
+	</head>
+    <body>
